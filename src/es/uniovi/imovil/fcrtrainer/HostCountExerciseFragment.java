4e399975914685b652c65c2ab4a4c8c36b7a8b9f
uo214815 <uo214815@uniovi.es>
2014-03-19 19:51:37 +0100

    Creacion clase HostCountFragment y cambio en la factoria añadiendo el return

---
+++ src/es/uniovi/imovil/fcrtrainer/HostCountExerciseFragment.java
@@ -0,0 +1,31 @@
+package es.uniovi.imovil.fcrtrainer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HostCountExerciseFragment extends BaseExerciseFragment{
+
+
+	private View mRootView;
+
+	//Constructor.
+	public HostCountExerciseFragment() 
+	{
+		
+	}
+	
+	public static HostCountExerciseFragment newInstance() 
+	{
+		HostCountExerciseFragment fragment = new HostCountExerciseFragment();
+		return fragment;
+	}
+	
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		mRootView=inflater.inflate(R.layout.fragment_protocol, container, false);
+		return mRootView;
+	}
+
+}
