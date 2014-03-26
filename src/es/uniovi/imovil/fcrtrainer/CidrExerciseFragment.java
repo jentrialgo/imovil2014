0041178fe059dc96da871e5793dca142bf92bd7b
AngelMdez <angelmendezcampal@gmail.com>
2014-03-26 01:26:23 +0100

    Revert "Revert "Modificado FragmentFactory,añadido fragmentCIDR,creado layout.""
    This reverts commit 3895da500d520627698a6b5d7b8e69ec9e5738eb.

---
+++ src/es/uniovi/imovil/fcrtrainer/CidrExerciseFragment.java
@@ -0,0 +1,30 @@
+package es.uniovi.imovil.fcrtrainer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CidrExerciseFragment extends BaseExerciseFragment{
+
+	public static CidrExerciseFragment newInstance() {
+		
+		CidrExerciseFragment fragment = new CidrExerciseFragment();
+		return fragment;
+	}
+	
+	public CidrExerciseFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View rootView;
+		rootView = inflater.inflate(R.layout.fragment_cidr, container, false);
+		
+		return rootView;
+	}
+
+}
