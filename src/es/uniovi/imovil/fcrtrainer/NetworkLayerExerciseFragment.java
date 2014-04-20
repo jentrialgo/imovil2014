4367ba7a929cbaa5745dd90302903d97505f7cad
Oscar Menéndez <oscarmdzgarcia@gmail.com>
2014-04-20 20:33:55 +0200

    Añadido modo entrenamiento funcional del ejercicio NetworkLayer. Implementado el diseño y la animación acordados.

--- src/es/uniovi/imovil/fcrtrainer/NetworkLayerExerciseFragment.java
+++ src/es/uniovi/imovil/fcrtrainer/NetworkLayerExerciseFragment.java
@@ -4,19 +4,33 @@ import java.util.HashMap;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class NetworkLayerExerciseFragment extends BaseExerciseFragment {
-	Button comprobar;
-	Spinner respuesta;
-	String[] preguntas;
-	String[] respuestas;
+	private TextView pregunta;
+	private RadioGroup opciones;
+	
+	private RadioButton rb_layer;
+	private RadioButton rb_network;
+	private RadioButton rb_transport;
+	private RadioButton rb_application;
 
+	private Button comprobar;
+	private Button solution;
+	private String[] preguntas;
+	private String[] respuestas;
+	private String rb_pressed="";
+	private int indice =0;
 
 	//constructores
 	public NetworkLayerExerciseFragment() 
@@ -31,50 +45,112 @@ public class NetworkLayerExerciseFragment extends BaseExerciseFragment {
 		NetworkLayerExerciseFragment fragment = new NetworkLayerExerciseFragment();
 		return fragment;
 	}
+	
+	
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
 			Bundle savedInstanceState) {
 	
 		View rootView;		
-		rootView =inflater.inflate(R.layout.fragment_layer, container, false);		
+		rootView =inflater.inflate(R.layout.fragment_layer, container, false);
+		//TextView para mostrar la pregunta
+		pregunta = (TextView) rootView.findViewById(R.id.textlayer);
+		
+		//Radiogrup
+		opciones = (RadioGroup) rootView.findViewById(R.id.layer_group);
+		rb_layer = (RadioButton) rootView.findViewById(R.id.link_layer);
+		rb_network = (RadioButton) rootView.findViewById(R.id.internet_layer);
+		rb_transport = (RadioButton) rootView.findViewById(R.id.transport_layer);
+		rb_application = (RadioButton) rootView.findViewById(R.id.application_layer);
+
+		
+		opciones.setOnCheckedChangeListener( new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup rGroup, int checkedId)
+            {
+            	 // TODO Auto-generated method stub
+                switch(checkedId){
+                case R.id.link_layer:
+                	rb_pressed = "Capa de enlace";
+                    break;
+                case R.id.internet_layer:
+                	rb_pressed = "Capa de internet";
+                	break;
+                case R.id.transport_layer:
+                	rb_pressed = "Capa de transporte";
+                	break;
+                case R.id.application_layer:
+                	rb_pressed = "Capa de aplicación";
+                	break;
+                }
+            	
+        		RadioButton checkedRadioButton = (RadioButton)opciones.findViewById(opciones.getCheckedRadioButtonId());
+                boolean checked = checkedRadioButton.isChecked();
+                Log.v("h","Checked:"+checked);
+            }
+        });
+			
+		//Buttons
 		comprobar = (Button) rootView.findViewById(R.id.button_layer);
-		setSpinnerContent( rootView );
+		solution = (Button) rootView.findViewById(R.id.button_solutionlayer);
+		
+		comprobar.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if(v.getId() == R.id.button_layer){
+					CompruebaRespuesta();
+				}
+			}
+		});
+		
+		solution.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if(v.getId() == R.id.button_solutionlayer){
+					Solucion();
+				}
+			}
+		});
+
+		//Arrays
 		preguntas = getResources().getStringArray(R.array.layer_exercise_questions);
 		respuestas = getResources().getStringArray(R.array.layer_exercise_answers);
-		HashMap<String, String> layerMap = new HashMap<String, String>();
-		layerMap.put(preguntas[0], "Capa1");
-		layerMap.put(preguntas[1], "Capa4");
-		layerMap.put(preguntas[2], "Capa3");
-		layerMap.put(preguntas[3], "Capa1");
-		layerMap.put(preguntas[4], "Capa3");
-		layerMap.put(preguntas[5], "Capa4");
-		layerMap.put(preguntas[6], "Capa1");
+		
+		pregunta.setText(preguntas[indice]);
+		
 		 
 		return rootView;
 	}
-	
-	public void onClick(View v) {
-		if(v.getId() == R.id.button_layer){
-			//CompruebaRespuesta();
-
+		
+	protected void Solucion() {
+		Log.v("g","entre en solución");
+		if (respuestas[indice].equals("Capa de enlace")){
+		rb_layer.setChecked(true);
 		}
-
-	}
-	
-	private void setSpinnerContent( View view ){
-		  respuesta = (Spinner) view.findViewById( R.id.spinner );
-		// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-               R.array.layer_exercise_answers, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        respuesta.setAdapter(adapter);
+		else if (respuestas[indice].equals("Capa de internet")){
+			rb_network.setChecked(true);
+		}
+		else if (respuestas[indice].equals("Capa de transporte")){
+			rb_transport.setChecked(true);
+		}
+		else {
+			rb_application.setChecked(true);
+		}
 	}
 
 
-
+	private void CompruebaRespuesta() {
+		if (rb_pressed.equals(respuestas[indice])){
+			showAnimationAnswer(true);
+			if (indice <5){
+				indice++;
+			}
+			else indice=0;
+			
+			pregunta.setText(preguntas[indice]);
+		}
+		else showAnimationAnswer(false);		
+	}			
 }
+
+
 
 
 
