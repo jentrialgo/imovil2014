4ddde637a72dfea5f06f2c91f7c547a71b8814ea
David Fernández <yo@naroh.es>
2014-05-02 04:23:58 +0200

    First approach to game mode

--- src/es/uniovi/imovil/fcrtrainer/HexadecimalExerciseFragment.java
+++ src/es/uniovi/imovil/fcrtrainer/HexadecimalExerciseFragment.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2014 Profesores y alumnos de la asignatura Informática Móvil de la EPI de Gijón
+Copyright 2014 Profesores y alumnos de la asignatura Inform√°tica M√≥vil de la EPI de Gij√≥n
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,10 +18,15 @@ limitations under the License.
 
 package es.uniovi.imovil.fcrtrainer;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
 
-import android.content.res.Configuration;
+import org.json.JSONException;
+
+import es.uniovi.imovil.fcrtrainer.highscores.HighscoreManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -42,9 +47,14 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 	private Button bSolution;
 	private TextView tvNumberToConvert;
 	private TextView tvTitle;
+	private TextView tvPoints;
 	private int numberToConvert;
 	private boolean tohex = true;
+	private boolean game = false;
+	private int gameCounter = 0;
+	private int pointsCounter = 0;
 	private static final int MAX_NUMBER_TO_CONVERT = 1000;
+	private static final int GAMEMODE_MAXQUESTIONS = 5;
 
 	public static HexadecimalExerciseFragment newInstance() {
 
@@ -70,6 +80,7 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 		tvNumberToConvert = (TextView) rootView
 				.findViewById(R.id.numbertoconvert);
 		tvTitle = (TextView) rootView.findViewById(R.id.exercisetitle);
+		tvPoints = (TextView) rootView.findViewById(R.id.tvpoints);
 
 		etAnswer.setOnEditorActionListener(new OnEditorActionListener() {
 
@@ -126,10 +137,7 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 			}
 		});
 
-		Log.i(getClass().getSimpleName(), "onViewCreated");
-
 		if (savedInstanceState != null) {
-			Log.i(getClass().getSimpleName(), "Inside savedInstanceState");
 			tohex = savedInstanceState.getBoolean("tohex");
 			numberToConvert = savedInstanceState.getInt("numbertoconvert");
 			updateUI();
@@ -138,13 +146,13 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 
 		return rootView;
 	}
-
+	
 	public void generateRandomNumber() {
 		Random randomGenerator = new Random();
 		numberToConvert = randomGenerator.nextInt(MAX_NUMBER_TO_CONVERT);
 		updateUI();
 	}
-
+	
 	public void updateUI() {
 		if (tohex)
 			tvNumberToConvert.setText(Integer.toBinaryString(numberToConvert));
@@ -154,23 +162,32 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 	}
 
 	/**
-	 * Checks if the answer is correct
+	 * Checks if the answer is correct. If the game mode is selected checks for the number of 
+	 * questions that already has been asked, in case that number is equal to the max number 
+	 * allowed calls endGame(). Also it adds a point for each correct answer in that mode. 
 	 * 
 	 * @param answer the user input
 	 */
 	public void isCorrect(String answer) {
+		
+		if(game){
+			gameCounter++;
+			if(gameCounter == GAMEMODE_MAXQUESTIONS) endGame();
+		}
 
 		etAnswer.setText("");
 
 		if (tohex) {
 			if (answer.equals(Integer.toHexString(numberToConvert))) {
 				showAnimationAnswer(true);
+				if(game) pointsCounter++;
 				generateRandomNumber();
 			} else
 				showAnimationAnswer(false);
 		} else {
 			if (answer.equals(Integer.toBinaryString(numberToConvert))) {
 				showAnimationAnswer(true);
+				if(game) pointsCounter++;
 				generateRandomNumber();
 			} else
 				showAnimationAnswer(false);
@@ -190,22 +207,53 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 		outState.putBoolean("tohex", tohex);
 		outState.putInt("numbertoconvert", numberToConvert);
 	}
+	
+	public void updatePoints(int points){
+		tvPoints.setText(String.valueOf(points));
+	}
+	
+	public void saveScore(int points){		
+		String user = getString(R.string.default_user_name);
+	
+		try {
+			HighscoreManager.addScore(getActivity().getApplicationContext(), points, 0, new Date(), user);
+		} catch (JSONException e) {
+			Log.v(getClass().getSimpleName(), "Error when saving score");
+		}
+	}
+	
+	public void setTrainingMode(boolean training){
+		if(training){
+			game = false;
+			bSolution.setVisibility(View.VISIBLE);
+			tvPoints.setVisibility(View.GONE);
+			gameCounter = 0;
+			pointsCounter = 0;
+		}else{
+			game = true;
+			bSolution.setVisibility(View.GONE);
+			tvPoints.setVisibility(View.VISIBLE);
+		}
+	}
 
 	@Override
 	void startGame() {
-		// TODO Auto-generated method stub
 		super.startGame();
+		setTrainingMode(false);
+		updatePoints(pointsCounter);
 	}
 
 	@Override
 	void cancelGame() {
-		// TODO Auto-generated method stub
 		super.cancelGame();
+		setTrainingMode(true);
 	}
 
 	@Override
 	void endGame() {
-		// TODO Auto-generated method stub
 		super.endGame();
+		pointsCounter = (int) getRemainingTimeMs() / 1000 + pointsCounter;
+		updatePoints(pointsCounter);
+		saveScore(pointsCounter);
 	}
 }
