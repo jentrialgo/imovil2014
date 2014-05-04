9316e8871bad11c43df7ce1416fd079d2d536643
David Fernández <yo@naroh.es>
2014-05-04 03:36:47 +0200

    Functional game mode

--- src/es/uniovi/imovil/fcrtrainer/HexadecimalExerciseFragment.java
+++ src/es/uniovi/imovil/fcrtrainer/HexadecimalExerciseFragment.java
@@ -18,8 +18,6 @@ limitations under the License.
 
 package es.uniovi.imovil.fcrtrainer;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
@@ -27,6 +25,7 @@ import java.util.Random;
 import org.json.JSONException;
 
 import es.uniovi.imovil.fcrtrainer.highscores.HighscoreManager;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -51,8 +50,8 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 	private int numberToConvert;
 	private boolean tohex = true;
 	private boolean game = false;
-	private int gameCounter = 0;
 	private int pointsCounter = 0;
+	private int askedQuestions = 1;
 	private static final int MAX_NUMBER_TO_CONVERT = 1000;
 	private static final int GAMEMODE_MAXQUESTIONS = 5;
 	private Random randomGenerator;
@@ -82,7 +81,7 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 				.findViewById(R.id.numbertoconvert);
 		tvTitle = (TextView) rootView.findViewById(R.id.exercisetitle);
 		tvPoints = (TextView) rootView.findViewById(R.id.tvpoints);
-		
+
 		randomGenerator = new Random();
 
 		etAnswer.setOnEditorActionListener(new OnEditorActionListener() {
@@ -149,12 +148,12 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 
 		return rootView;
 	}
-	
+
 	public void generateRandomNumber() {
 		numberToConvert = randomGenerator.nextInt(MAX_NUMBER_TO_CONVERT);
 		updateUI();
 	}
-	
+
 	public void updateUI() {
 		if (tohex)
 			tvNumberToConvert.setText(Integer.toBinaryString(numberToConvert));
@@ -164,32 +163,31 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 	}
 
 	/**
-	 * Checks if the answer is correct. If the game mode is selected checks for the number of 
-	 * questions that already has been asked, in case that number is equal to the max number 
-	 * allowed calls endGame(). Also it adds a point for each correct answer in that mode. 
+	 * Checks if the answer is correct. If the game mode is selected checks for
+	 * the number of questions that already has been asked, in case that number
+	 * is equal to the max number allowed calls endGame(). Also it adds a point
+	 * for each correct answer in that mode.
 	 * 
-	 * @param answer the user input
+	 * @param answer
+	 *            the user input
 	 */
 	public void isCorrect(String answer) {
-		
-		if(game){
-			gameCounter++;
-			if(gameCounter == GAMEMODE_MAXQUESTIONS) endGame();
-		}
 
 		etAnswer.setText("");
 
 		if (tohex) {
 			if (answer.equals(Integer.toHexString(numberToConvert))) {
 				showAnimationAnswer(true);
-				if(game) pointsCounter++;
+				if (game)
+					updateGameState();
 				generateRandomNumber();
 			} else
 				showAnimationAnswer(false);
 		} else {
 			if (answer.equals(Integer.toBinaryString(numberToConvert))) {
 				showAnimationAnswer(true);
-				if(game) pointsCounter++;
+				if (game)
+					updateGameState();
 				generateRandomNumber();
 			} else
 				showAnimationAnswer(false);
@@ -209,33 +207,45 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 		outState.putBoolean("tohex", tohex);
 		outState.putInt("numbertoconvert", numberToConvert);
 	}
-	
-	public void updatePoints(int points){
-		tvPoints.setText(String.valueOf(points));
-	}
-	
-	public void saveScore(int points){		
+
+	public void saveScore(int points) {
 		String user = getString(R.string.default_user_name);
-	
+
 		try {
-			HighscoreManager.addScore(getActivity().getApplicationContext(), points, 0, new Date(), user);
+			HighscoreManager.addScore(getActivity().getApplicationContext(),
+					points, R.string.hexadecimal, new Date(), user);
 		} catch (JSONException e) {
 			Log.v(getClass().getSimpleName(), "Error when saving score");
 		}
 	}
-	
-	public void setTrainingMode(boolean training){
-		if(training){
+
+	public void setTrainingMode(boolean training) {
+		if (training) {
 			game = false;
 			bSolution.setVisibility(View.VISIBLE);
 			tvPoints.setVisibility(View.GONE);
-			gameCounter = 0;
-			pointsCounter = 0;
-		}else{
+			resetGameState();
+		} else {
 			game = true;
 			bSolution.setVisibility(View.GONE);
 			tvPoints.setVisibility(View.VISIBLE);
 		}
+	}
+
+	public void updateGameState() {
+		pointsCounter++;
+		updatePoints(pointsCounter);
+		if (pointsCounter == GAMEMODE_MAXQUESTIONS)
+			endGame();
+	}
+
+	public void resetGameState() {
+		pointsCounter = 0;
+		tvPoints.setVisibility(View.GONE);
+	}
+
+	public void updatePoints(int points) {
+		tvPoints.setText(String.valueOf(points));
 	}
 
 	@Override
@@ -254,8 +264,30 @@ public class HexadecimalExerciseFragment extends BaseExerciseFragment {
 	@Override
 	void endGame() {
 		super.endGame();
-		pointsCounter = (int) getRemainingTimeMs() / 1000 + pointsCounter;
-		updatePoints(pointsCounter);
+
+		int remainingTime = (int) getRemainingTimeMs() / 1000;
+		pointsCounter = pointsCounter + remainingTime;
+
+		showEndGameDialog(remainingTime);
+
 		saveScore(pointsCounter);
+		resetGameState();
+		setTrainingMode(true);
 	}
+
+	public void showEndGameDialog(int remainingTime) {
+		AlertDialog.Builder abuilder = new AlertDialog.Builder(getActivity());
+		abuilder.setTitle(getString(R.string.gameisover));
+
+		if (remainingTime > 0)
+			abuilder.setMessage(String.format(
+					getString(R.string.gameisoverexp), remainingTime,
+					pointsCounter));
+		else
+			abuilder.setMessage(String.format(
+					getString(R.string.lost_time_over), pointsCounter));
+
+		abuilder.create().show();
+	}
+
 }
