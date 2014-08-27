ffd438d37a1507881d2d0e187f9f2c5dd447e3c4
jentrialgo <jentrialgo@gmail.com>
2014-08-27 12:49:09 +0200

    Refactoring: nombres y formateo del código

--- src/es/uniovi/imovil/fcrtrainer/NetworkMaskExerciseFragment.java
+++ src/es/uniovi/imovil/fcrtrainer/NetworkMaskExerciseFragment.java
@@ -9,227 +9,182 @@ import es.uniovi.imovil.fcrtrainer.highscores.HighscoreManager;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 
-
-
-public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements OnClickListener{
-	
-	
-	
-	private TextView enunciado;
-	private EditText solucion;
-	private Button botonR;
-	private Button botonC;
-	private String[]preguntas;
-	private String[]respuestas;
-	private ImageView imagen;
-	private int i;
-	private Random r;
-	private Handler handler;
-	private boolean gameMode = false;
-	private int points;
-	private int currentQuestionCounter = 1;
+public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements
+		OnClickListener {
 	private static final int POINTS_FOR_QUESTION = 10;
 	private static final int MAX_QUESTIONS = 5;
-	private static final long GAME_DURATION_MS = 5 * 1000 * 60; 
-	private boolean won=false;
-	private View rootView;
-	
-	
+	private static final long GAME_DURATION_MS = 5 * 1000 * 60;
+
+	private TextView mQuestionTitle;
+	private EditText mSolution;
+	private Button mButtonR;
+	private Button mButtonC;
+	private String[] mQuestions;
+	private String[] mAnswers;
+	private ImageView mImage;
+
+	private int mCurrentQuestion;
+	private Random mRandom;
+	private Handler mHandler;
+	private boolean mGameMode = false;
+	private int mPoints;
+	private int mCurrentQuestionCounter = 1;
+	private boolean mWon = false;
+	private View mRootView;
+
 	public static NetworkMaskExerciseFragment newInstance() {
-		
 		NetworkMaskExerciseFragment fragment = new NetworkMaskExerciseFragment();
 		return fragment;
 	}
-	
+
 	public NetworkMaskExerciseFragment() {
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		
-		rootView = inflater.inflate(R.layout.fragment_networkmask, container, false);
-		r=new Random();
-		i=r.nextInt(5);
-		preguntas=new String[5];
-		respuestas=new String[5];
-		handler=new Handler();
-		imagen=(ImageView)rootView.findViewById(R.id.resultimage);
-		enunciado=(TextView)rootView.findViewById(R.id.TextEnun2);
-		solucion=(EditText)rootView.findViewById(R.id.respuesta);
-		botonR=(Button)rootView.findViewById(R.id.button1);
-		botonC=(Button)rootView.findViewById(R.id.button2);
-		preguntas=getResources().getStringArray(R.array.preguntas);
-		
-		respuestas=getResources().getStringArray(R.array.respuestas);
-		
-		
-		enunciado.setText(preguntas[i]);
-		botonR.setOnClickListener(this);
-		botonC.setOnClickListener(this);
-		
-		
-		
-		return rootView;
+		mRootView = inflater.inflate(R.layout.fragment_networkmask, container,
+				false);
+		mRandom = new Random();
+		mCurrentQuestion = mRandom.nextInt(MAX_QUESTIONS);
+		mQuestions = new String[MAX_QUESTIONS];
+		mAnswers = new String[MAX_QUESTIONS];
+		mHandler = new Handler();
+		mImage = (ImageView) mRootView.findViewById(R.id.resultimage);
+		mQuestionTitle = (TextView) mRootView.findViewById(R.id.TextEnun2);
+		mSolution = (EditText) mRootView.findViewById(R.id.respuesta);
+		mButtonR = (Button) mRootView.findViewById(R.id.button1);
+		mButtonC = (Button) mRootView.findViewById(R.id.button2);
+		mQuestions = getResources().getStringArray(R.array.preguntas);
+
+		mAnswers = getResources().getStringArray(R.array.respuestas);
+
+		mQuestionTitle.setText(mQuestions[mCurrentQuestion]);
+		mButtonR.setOnClickListener(this);
+		mButtonC.setOnClickListener(this);
+
+		return mRootView;
 	}
-	
-	
-	public void onClick(View v){
-		
-		if(v.getId()==R.id.button1){
-				Log.i("EOOOO", solucion.getText().toString());
-				Log.i("EIIIII", respuestas[i]);
-			if(solucion.getText().toString().equals(respuestas[i])){
-				//Toast.makeText(getActivity(), "correcto", 2000).show();
-				//imagen.setImageResource(R.drawable.correct);
-				//imagen.setVisibility(View.VISIBLE);
-				if(gameMode){
+
+	public void onClick(View v) {
+		if (v.getId() == R.id.button1) {
+			if (mSolution.getText().toString()
+					.equals(mAnswers[mCurrentQuestion])) {
+				// Toast.makeText(getActivity(), "correcto", 2000).show();
+				// imagen.setImageResource(R.drawable.correct);
+				// imagen.setVisibility(View.VISIBLE);
+				if (mGameMode) {
 					gameModeControl();
 				}
 				showAnimationAnswer(true);
-				handler.postDelayed(new Runnable(){
-					public void run(){
-						imagen.setImageResource(0);	
-						i=r.nextInt(5);
-						enunciado.setText(preguntas[i]);
-						solucion.setText("");
+				mHandler.postDelayed(new Runnable() {
+					public void run() {
+						mImage.setImageResource(0);
+						mCurrentQuestion = mRandom.nextInt(5);
+						mQuestionTitle.setText(mQuestions[mCurrentQuestion]);
+						mSolution.setText("");
 					}
 				}, 1500);
-				
-				
-			}else{
-				
-				//imagen.setImageResource(R.drawable.incorrect);
-				//imagen.setVisibility(View.VISIBLE);
+			} else {
 				showAnimationAnswer(false);
-				handler.postDelayed(new Runnable(){
-					public void run(){
-						imagen.setImageResource(0);	
-						i=r.nextInt(5);
-						enunciado.setText(preguntas[i]);
-						solucion.setText("");
+				mHandler.postDelayed(new Runnable() {
+					public void run() {
+						mImage.setImageResource(0);
+						mCurrentQuestion = mRandom.nextInt(5);
+						mQuestionTitle.setText(mQuestions[mCurrentQuestion]);
+						mSolution.setText("");
 					}
 				}, 1500);
-				
-				
-				
-				
+
 			}
-				
-				
-				
+
 		}
-		
-		
-		if(v.getId()==R.id.button2){
-			
-			solucion.setText(respuestas[i]);
-		
-			handler.postDelayed(new Runnable(){
-				public void run(){
-						
-					i=r.nextInt(5);
-					enunciado.setText(preguntas[i]);
-					solucion.setText("");
+
+		if (v.getId() == R.id.button2) {
+			mSolution.setText(mAnswers[mCurrentQuestion]);
+
+			mHandler.postDelayed(new Runnable() {
+				public void run() {
+
+					mCurrentQuestion = mRandom.nextInt(5);
+					mQuestionTitle.setText(mQuestions[mCurrentQuestion]);
+					mSolution.setText("");
 				}
-			}, 1500);	
-			
-			
-		
-	
-		
-		
+			}, 1500);
+
+		}
 	}
-	
-	
-   }
-	
-	
-	
+
 	private void gameModeControl() {
 		increasePoints(POINTS_FOR_QUESTION);
 
-		if (currentQuestionCounter >= MAX_QUESTIONS) {
+		if (mCurrentQuestionCounter >= MAX_QUESTIONS) {
 			// won
-			this.won = true;
+			this.mWon = true;
 			this.endGame();
 
 		}
 
-		if (currentQuestionCounter < MAX_QUESTIONS && getRemainingTimeMs() <= 0) {
+		if (mCurrentQuestionCounter < MAX_QUESTIONS
+				&& getRemainingTimeMs() <= 0) {
 			// lost --> no time left...
-			this.won = false;
+			this.mWon = false;
 			this.endGame();
 		}
-		currentQuestionCounter++;
+		mCurrentQuestionCounter++;
 	}
 
 	private void increasePoints(int val) {
-		this.points = this.points + val;
-		updatePointsTextView(this.points);
+		this.mPoints = this.mPoints + val;
+		updatePointsTextView(this.mPoints);
 	}
 
 	private void updatePointsTextView(int p) {
-		TextView tvPoints = (TextView) rootView.findViewById(R.id.points);
-		tvPoints.setText(getResources().getString(R.string.points)+ " "+ String.valueOf(p));
+		TextView tvPoints = (TextView) mRootView.findViewById(R.id.points);
+		tvPoints.setText(getResources().getString(R.string.points) + " "
+				+ String.valueOf(p));
 	}
-
-
-	
-
 
 	@Override
 	public void startGame() {
 		setGameDuration(GAME_DURATION_MS);
-		
+
 		// set starting points of textview
-		updatePointsTextView(0); 
+		updatePointsTextView(0);
 
 		super.startGame();
 		updateToGameMode();
 	}
 
 	private void updateToGameMode() {
-		gameMode = true;
+		mGameMode = true;
 
-		//	newQuestion();
-
-		Button solution = (Button) rootView.findViewById(R.id.button2);
+		Button solution = (Button) mRootView.findViewById(R.id.button2);
 		solution.setVisibility(View.INVISIBLE);
-		//Button change = (Button) rootView.findViewById(R.id.change);
-		//change.setVisibility(View.INVISIBLE);
 
-		TextView points = (TextView) rootView.findViewById(R.id.points);
+		TextView points = (TextView) mRootView.findViewById(R.id.points);
 		points.setVisibility(View.VISIBLE);
-
 	}
 
 	private void updateToTrainMode() {
-		gameMode = false;
+		mGameMode = false;
 
-		Button solution = (Button) rootView.findViewById(R.id.button2);
+		Button solution = (Button) mRootView.findViewById(R.id.button2);
 		solution.setVisibility(View.VISIBLE);
-		//Button change = (Button) rootView.findViewById(R.id.change);
-		//change.setVisibility(View.VISIBLE);
 
-		TextView points = (TextView) rootView.findViewById(R.id.points);
+		TextView points = (TextView) mRootView.findViewById(R.id.points);
 		points.setVisibility(View.INVISIBLE);
 	}
 
@@ -241,12 +196,13 @@ public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements
 
 	@Override
 	void endGame() {
-		//convert to seconds
-		int remainingTimeInSeconds = (int) super.getRemainingTimeMs() / 1000; 
-		//every remaining second gives one extra point.
-		this.points = (int) (this.points + remainingTimeInSeconds);
+		// convert to seconds
+		int remainingTimeInSeconds = (int) super.getRemainingTimeMs() / 1000;
 
-		if (this.won)
+		// every remaining second gives one extra point.
+		this.mPoints = (int) (this.mPoints + remainingTimeInSeconds);
+
+		if (this.mWon)
 			savePoints();
 
 		dialogGameOver();
@@ -259,9 +215,9 @@ public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements
 	}
 
 	private void reset() {
-		this.points = 0;
-		this.currentQuestionCounter = 0;
-		this.won = false;
+		this.mPoints = 0;
+		this.mCurrentQuestionCounter = 0;
+		this.mWon = false;
 
 		updatePointsTextView(0);
 	}
@@ -269,9 +225,8 @@ public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements
 	private void savePoints() {
 		String username = getResources().getString(R.string.default_user_name);
 		try {
-
 			HighscoreManager.addScore(getActivity().getApplicationContext(),
-					this.points, R.string.binary, new Date(), username);
+					this.mPoints, R.string.binary, new Date(), username);
 
 		} catch (JSONException e) {
 			e.printStackTrace();
@@ -282,10 +237,10 @@ public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements
 	private void dialogGameOver() {
 		String message = getResources().getString(R.string.lost);
 
-		if (this.won) {
+		if (this.mWon) {
 			message = getResources().getString(R.string.won) + " "
 					+ getResources().getString(R.string.points) + " "
-					+ this.points;
+					+ this.mPoints;
 		}
 
 		Builder alert = new AlertDialog.Builder(getActivity());
@@ -302,13 +257,4 @@ public class NetworkMaskExerciseFragment extends BaseExerciseFragment implements
 
 	}
 
-
-	
-	
-	
-	
-	
-	
-	
 }
-
