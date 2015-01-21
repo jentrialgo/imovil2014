7d0639f9c1aa8bccf13adfb0d1a0e7df1ab9c871
jentrialgo <jentrialgo@gmail.com>
2015-01-21 09:10:31 +0100

    Usar Toolbar en vez de ActionBar
    Los iconos todavía aparecen oscuros

--- src/es/uniovi/imovil/fcrtrainer/MainActivity.java
+++ src/es/uniovi/imovil/fcrtrainer/MainActivity.java
@@ -32,6 +32,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -69,6 +70,9 @@ public class MainActivity extends ActionBarActivity implements
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
 		mTitle = getTitle();
 		mDrawerTitle = mTitle;
